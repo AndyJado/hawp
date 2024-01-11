72d8f72baa5d618fb84ef9bf09e87c7e3dc1e85a
AndyJado <101876416+AndyJado@users.noreply.github.com>
2024-01-11 13:06:15 +0800

    4 args pd.sh

--- pd.sh
+++ pd.sh
@@ -1,1 +1,1 @@
-python -m hawp.ssl.predict --device cpu --ckpt checkpoints/hawpv3-imagenet-03a84.pth --cta $3 --threshold $2 --img $1 --saveto ./ --ext json --disable-show
+python -m hawp.ssl.predict --device cpu --ckpt checkpoints/hawpv3-imagenet-03a84.pth --cta $3 --threshold $4 --img $1 --saveto $2 --ext json --disable-show
