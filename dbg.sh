d801379aea4fb0592a115283be8d930728b0bedf
AndyJado <101876416+AndyJado@users.noreply.github.com>
2024-01-12 14:02:38 +0800

    dbg

--- dbg.sh
+++ dbg.sh
@@ -1,1 +1,1 @@
-python -m hawp.ssl.predict --device cpu --ckpt checkpoints/hawpv3-imagenet-03a84.pth --cta $3 --threshold $2 --img $1 --saveto ./test/ --ext json --disable-show
+python -m hawp.ssl.predict --device mps --ckpt checkpoints/hawpv3-imagenet-03a84.pth --cta 30 --threshold 0.05 --img ./test/duh.png --saveto ./test/ --ext json --disable-show
--- hawp/base/show/painters.py
+++ hawp/base/show/painters.py
@@ -54,10 +54,7 @@ class HAWPainter:
         
         if self.marker_size is None:
             self.marker_size = max(1, int(self.line_width * 0.5))
-
     
-
-
 
     def draw_segs(self, ax, line_segments, *,
             edge_color = None, vertex_color = None):
@@ -72,14 +69,14 @@ class HAWPainter:
         # [x1,y1,x2,y2]
         for (x1,y1,x2,y2) in line_segments:
             ax.plot([x1,x2],[y1,y2],'-',color=edge_color)
-        ax.plot(line_segments[:,0],line_segments[:,1],'o',color=vertex_color)
-        ax.plot(line_segments[:,2],line_segments[:,3],'o',color=vertex_color)
+        ax.plot(line_segments[:,0],line_segments[:,1],'.',color=vertex_color)
+        ax.plot(line_segments[:,2],line_segments[:,3],'.',color=vertex_color)
         idx = 0
         for line in line_segments:
             idx += 1
             x = (line[0] + line[2]) / 2
             y = (line[1] + line[3]) / 2
-            ax.text(x,y,str(idx),color='red')
+            ax.text(x,y,str(idx),color='brown',horizontalalignment='center',verticalalignment='center',fontsize=18)
             # print(idx)
 
     def trianglerm(self, wireframe,indices,kbar):
